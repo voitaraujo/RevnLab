65641abc10951d28f31d7fb77076d0c83d55f29b
voitaraujo <voitilaaraujo@gmail.com>
2022-02-21 23:31:41 -0300

    21.02

--- backend/src/controllers/StoragesController.ts
+++ backend/src/controllers/StoragesController.ts
@@ -12,149 +12,149 @@ interface IFaltaEmDL { DLCod: string, Refdt: string, FaltamProdutos: number }
 interface IFaltaEmEQ { Refdt: string, CHAPA: string, Faltam: number }
 
 export default {
-    async Show(req: Request, res: Response) {
-        const token = req.get('Authorization')
+  async Show(req: Request, res: Response) {
+    const token = req.get('Authorization')
 
-        const verified = <IToken>decryptToken(token!)
-        const mesAnterior = moment().subtract(1, 'month').startOf('month').format("YYYY-MM-DD hh:mm:ss").replace('12:', '00:')
+    const verified = <IToken>decryptToken(token!)
+    const mesAnterior = moment().subtract(1, 'month').startOf('month').format("YYYY-MM-DD hh:mm:ss").replace('12:', '00:')
 
-        const storagesCab = await getRepository(Storages).find({
-            select: ['DLCod', 'DLNome', 'Filial'],
-            where: {
-                GestorCod: verified.supervisor_code,
-                DLStatus: 'S',
-            },
-            order: {
-                DLCod: 'ASC'
-            }
-        })
+    const storagesCab = await getRepository(Storages).find({
+      select: ['DLCod', 'DLNome', 'Filial'],
+      where: {
+        GestorCod: verified.supervisor_code,
+        DLStatus: 'S',
+      },
+      order: {
+        DLCod: 'ASC'
+      }
+    })
 
-        const faltamStorages = await getCurrentMonthStorageMovInfo(verified.supervisor_code, mesAnterior)
+    const faltamStorages = await getCurrentMonthStorageMovInfo(verified.supervisor_code, mesAnterior)
 
-        let storages: { DLCod: string, DLNome: string, Filial: string, Faltam: number }[] = []
+    let storages: { DLCod: string, DLNome: string, Filial: string, Faltam: number }[] = []
 
-        storagesCab.forEach(storage => {
-            let found = false
-            for (let i = 0; i < faltamStorages.length; i++) {
-                if (storage.DLCod === faltamStorages[i].DLCod) {
-                    storages.push(Object.assign({ ...storage }, { Faltam: faltamStorages[i].Faltam }))
-                    found = true
-                    break
-                }
-            }
-            if (!found) {
-                storages.push(Object.assign({ ...storage }, { Faltam: 0 }))
-            }
-            found = false
-        })
+    storagesCab.forEach(storage => {
+      let found = false
+      for (let i = 0; i < faltamStorages.length; i++) {
+        if (storage.DLCod === faltamStorages[i].DLCod) {
+          storages.push(Object.assign({ ...storage }, { Faltam: faltamStorages[i].Faltam }))
+          found = true
+          break
+        }
+      }
+      if (!found) {
+        storages.push(Object.assign({ ...storage }, { Faltam: 0 }))
+      }
+      found = false
+    })
 
-        storages ? res.status(200).send({ storages }) : res.status(400).send({
-            message: 'Error while querying database'
-        })
+    storages ? res.status(200).send({ storages }) : res.status(400).send({
+      message: 'Error while querying database'
+    })
 
-        return res
-    },
+    return res
+  },
 
-    async See(req: Request, res: Response) {
-        const token = req.get('Authorization')
-        const DLid = req.params.DL
-        const Filial = req.params.Filial
+  async See(req: Request, res: Response) {
+    const token = req.get('Authorization')
+    const DLid = req.params.DL
+    const Filial = req.params.Filial
 
-        const verified = <IToken>decryptToken(token!)
+    const verified = <IToken>decryptToken(token!)
 
-        const storage = verified && await getRepository(Storages).find({
-            where: {
-                GestorCod: verified.supervisor_code,
-                DLCod: DLid,
-                Filial: Filial
-            }
-        })
+    const storage = verified && await getRepository(Storages).find({
+      where: {
+        GestorCod: verified.supervisor_code,
+        DLCod: DLid,
+        Filial: Filial
+      }
+    })
 
-        const ProdsFaltamDL = await getPastStorageMovInfo(Filial, DLid, verified.supervisor_code)
-        const ProdsFaltamEQ = await getPastStorageMachinesMovInfo(Filial, DLid, verified.supervisor_code)
+    const ProdsFaltamDL = await getPastStorageMovInfo(Filial, DLid, verified.supervisor_code)
+    const ProdsFaltamEQ = await getPastStorageMachinesMovInfo(Filial, DLid, verified.supervisor_code)
 
-        const completeStorage = storage && storage.length > 0 ? {
-            ...storage[0],
-            pastMonthsDLInv: ProdsFaltamDL,
-            pastMonthsDLEqInv: ProdsFaltamEQ
-        } : false
+    const completeStorage = storage && storage.length > 0 ? {
+      ...storage[0],
+      pastMonthsDLInv: ProdsFaltamDL,
+      pastMonthsDLEqInv: ProdsFaltamEQ
+    } : false
 
-        completeStorage ? res.status(200).send(completeStorage) : res.status(400).send({
-            message: 'Error while querying database'
-        })
+    completeStorage ? res.status(200).send(completeStorage) : res.status(400).send({
+      message: 'Error while querying database'
+    })
 
-        return res
-    }
+    return res
+  }
 }
 
 const getPastStorageMovInfo = async (filial: string, DLId: string, GestorCod: string): Promise<IFaltaEmDL[]> => {
-    const RawQuery_MovStorage = getRepository(MovStorages).createQueryBuilder();
+  const RawQuery_MovStorage = getRepository(MovStorages).createQueryBuilder();
 
-    RawQuery_MovStorage.select("DLCod")
-        .addSelect("Refdt")
-        .addSelect("COUNT(PROD)", "FaltamProdutos")
-        .where(`Qtd IS NULL AND Filial = '${filial}' AND DLCod = '${DLId}' AND GestorCod = '${GestorCod}'`)
-        .groupBy("Refdt")
-        .addGroupBy("DLCod")
-        .addGroupBy("GestorCod")
-        .orderBy('Refdt', 'DESC');
+  RawQuery_MovStorage.select("DLCod")
+    .addSelect("Refdt")
+    .addSelect("COUNT(PROD)", "FaltamProdutos")
+    .where(`Qtd IS NULL AND Filial = '${filial}' AND DLCod = '${DLId}' AND GestorCod = '${GestorCod}'`)
+    .groupBy("Refdt")
+    .addGroupBy("DLCod")
+    .addGroupBy("GestorCod")
+    .orderBy('Refdt', 'DESC');
 
-    const _retorno = await RawQuery_MovStorage.getRawMany<IFaltaEmDL>()
+  const _retorno = await RawQuery_MovStorage.getRawMany<IFaltaEmDL>()
 
-    return _retorno
+  return _retorno
 }
 
 const getPastStorageMachinesMovInfo = async (filial: string, DLId: string, GestorCod: string): Promise<{ Ref: string, Eqs: IFaltaEmEQ[] }[]> => {
-    const RawQuery_MovEq = getRepository(MovMachines).createQueryBuilder();
+  const RawQuery_MovEq = getRepository(MovMachines).createQueryBuilder();
 
-    RawQuery_MovEq.select("Refdt")
-        .addSelect("CHAPA")
-        .addSelect("COUNT(PROD)", "Faltam")
-        .where(`Qtd is null and Filial = '${filial}' and DLCod = '${DLId}' AND GestorCod = '${GestorCod}'`)
-        .groupBy("DLCod")
-        .addGroupBy("CHAPA")
-        .addGroupBy("Refdt")
-        .orderBy('Refdt', 'DESC');
+  RawQuery_MovEq.select("Refdt")
+    .addSelect("CHAPA")
+    .addSelect("COUNT(PROD)", "Faltam")
+    .where(`Qtd is null and Filial = '${filial}' and DLCod = '${DLId}' AND GestorCod = '${GestorCod}'`)
+    .groupBy("DLCod")
+    .addGroupBy("CHAPA")
+    .addGroupBy("Refdt")
+    .orderBy('Refdt', 'DESC');
 
-    const UnInformedDLEqsProdsQtds = await RawQuery_MovEq.getRawMany<IFaltaEmEQ>()
+  const UnInformedDLEqsProdsQtds = await RawQuery_MovEq.getRawMany<IFaltaEmEQ>()
 
 
-    let _ref: string[] = []
-    let _retorno: { Ref: string, Eqs: IFaltaEmEQ[] }[] = []
+  let _ref: string[] = []
+  let _retorno: { Ref: string, Eqs: IFaltaEmEQ[] }[] = []
 
-    //crio todas as refs em um array pra consultar mais tarde
-    UnInformedDLEqsProdsQtds.forEach(EQ => {
-        if (_ref.indexOf(moment(EQ.Refdt).toISOString()) === -1) {
-            _ref.push(moment(EQ.Refdt).toISOString())
-        }
+  //crio todas as refs em um array pra consultar mais tarde
+  UnInformedDLEqsProdsQtds.forEach(EQ => {
+    if (_ref.indexOf(moment(EQ.Refdt).toISOString()) === -1) {
+      _ref.push(moment(EQ.Refdt).toISOString())
+    }
+  })
+
+  //crio nessa variavel um protótipo do objeto que eu quero no final
+  _ref.forEach(data => {
+    _retorno.push({ Ref: moment(data).format('MMMM'), Eqs: [] })
+  })
+
+  UnInformedDLEqsProdsQtds.forEach(EQ => {
+    _retorno.forEach(data => {
+      if (data.Ref === moment(EQ.Refdt).format('MMMM')) {
+        data.Eqs.push(EQ)
+      }
     })
+  })
 
-    //crio nessa variavel um protótipo do objeto que eu quero no final
-    _ref.forEach(data => {
-        _retorno.push({ Ref: moment(data).format('MMMM'), Eqs: [] })
-    })
-
-    UnInformedDLEqsProdsQtds.forEach(EQ => {
-        _retorno.forEach(data => {
-            if (data.Ref === moment(EQ.Refdt).format('MMMM')) {
-                data.Eqs.push(EQ)
-            }
-        })
-    })
-
-    return _retorno
+  return _retorno
 }
 
 const getCurrentMonthStorageMovInfo = async (GestorCod: string, Referencia: string): Promise<{ DLCod: string, Faltam: number }[]> => {
-    const RawQuery_MovStorage = getRepository(MovStorages).createQueryBuilder();
+  const RawQuery_MovStorage = getRepository(MovStorages).createQueryBuilder();
 
-    RawQuery_MovStorage.select("DLCod")
-        .addSelect("COUNT(PROD)", "Faltam")
-        .where(`Qtd IS NULL AND GestorCod = '${GestorCod}' AND Refdt = convert(smalldatetime, '${Referencia}', 101)`)
-        .groupBy("DLCod")
-        .orderBy('DLCod', 'DESC');
+  RawQuery_MovStorage.select("DLCod")
+    .addSelect("COUNT(PROD)", "Faltam")
+    .where(`Qtd IS NULL AND GestorCod = '${GestorCod}' AND Refdt = convert(smalldatetime, '${Referencia}', 101)`)
+    .groupBy("DLCod")
+    .orderBy('DLCod', 'DESC');
 
-    const _retorno = await RawQuery_MovStorage.getRawMany<{ DLCod: string, Faltam: number }>()
+  const _retorno = await RawQuery_MovStorage.getRawMany<{ DLCod: string, Faltam: number }>()
 
-    return _retorno
+  return _retorno
 }
